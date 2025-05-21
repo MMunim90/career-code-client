e9432d14cf0f16d8ef1c1e7696fda803b7dcda5b
ProgrammingHero1 <programming.hero1@gmail.com>
2025-05-21 11:23:28 +0200

    added signin signout and auth state change observer

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,6 +4,7 @@ import {
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
 import Register from "../pages/Register/Register";
+import SignIn from "../pages/SignIn/SignIn";
 
 const router = createBrowserRouter([
     {
@@ -15,8 +16,12 @@ const router = createBrowserRouter([
                 Component: Home
             },
             {
-                path: '/register',
+                path: 'register',
                 Component: Register
+            },
+            {
+                path: 'signIn',
+                Component: SignIn
             }
         ]
     },
