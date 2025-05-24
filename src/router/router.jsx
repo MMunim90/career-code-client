d85dd0b308fa76b518d9f6bd7bd3e3b10c797b42
ProgrammingHero1 <programming.hero1@gmail.com>
2025-05-24 22:26:58 +0200

    display hot jobs card

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,6 +5,7 @@ import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
 import Register from "../pages/Register/Register";
 import SignIn from "../pages/SignIn/SignIn";
+import JobDetails from "../pages/JobDetails/JobDetails";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,11 @@ const router = createBrowserRouter([
                 index: true,
                 Component: Home
             },
+            {
+                path:'/jobs/:id',
+                Component: JobDetails
+            }
+            ,
             {
                 path: 'register',
                 Component: Register
