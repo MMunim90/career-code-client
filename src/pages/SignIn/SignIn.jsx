4a4b8c0bfc0c1da6883e917bfd37671ba1279c1b
ProgrammingHero1 <programming.hero1@gmail.com>
2025-05-24 23:49:24 +0200

    auth redirect

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -3,40 +3,46 @@ import { AuthContext } from '../../contexts/AuthContext/AuthContext';
 import signInLottie from '../../assets/lotties/signIn.json'
 import Lottie from 'lottie-react';
 import SocialLogin from '../Shared/SocialLogin';
+import { useLocation, useNavigate } from 'react-router';
 
 const SignIn = () => {
+  const { signInUser } = use(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state || '/';
 
-    const {signInUser} = use(AuthContext);
-    
-        const handleSignIn = e =>{
-            e.preventDefault();
-            const form = e.target;
-            const email = form.email.value;
-            const password = form.password.value;
-            console.log(email, password)
-    
-            // sign in user
-           signInUser(email, password)
-           .then(result =>{
-            console.log(result.user)
-           })
-           .catch(error => {
-            console.log(error)
-           })
-    
-    
-        } 
+  console.log('location in sign in page', location)
 
-    return (
-        <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <Lottie style={{width: '200px'}} animationData={signInLottie}  loop={true} ></Lottie>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
+  const handleSignIn = e => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password)
+
+    // sign in user
+    signInUser(email, password)
+      .then(result => {
+        console.log(result.user);
+        navigate(from);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+
+  }
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <Lottie style={{ width: '200px' }} animationData={signInLottie} loop={true} ></Lottie>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
             <h1 className="text-5xl font-bold">Sign In now!</h1>
-              <form onSubmit={handleSignIn}>
+            <form onSubmit={handleSignIn}>
               <fieldset className="fieldset">
                 <label className="label">Email</label>
                 <input type="email" name="email" className="input" placeholder="Email" />
@@ -46,13 +52,13 @@ const SignIn = () => {
                 <button className="btn btn-neutral mt-4">Sign In</button>
               </fieldset>
 
-              </form>
-              <SocialLogin></SocialLogin>
-            </div>
+            </form>
+            <SocialLogin from={from}></SocialLogin>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default SignIn;
