60a61b6cf50e867f5dfa45030a7a728bece31992
ProgrammingHero1 <programming.hero1@gmail.com>
2025-05-25 08:24:10 +0200

    show my applications in a tabular format

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -1,13 +1,20 @@
 import React, { Suspense } from 'react';
 import ApplicationStats from './ApplicationStats';
 import ApplicationList from './ApplicationList';
+import useAuth from '../../hooks/useAuth';
+import { myApplicationsPromise } from '../../api/applicstionsApi';
 
 const MyApplications = () => {
+
+    const { user } = useAuth();
+
     return (
         <div>
             <ApplicationStats></ApplicationStats>
             <Suspense fallback={'loading your applications'}>
-                <ApplicationList></ApplicationList>
+                <ApplicationList
+                    myApplicationsPromise={myApplicationsPromise(user.email)}
+                ></ApplicationList>
             </Suspense>
         </div>
     );
