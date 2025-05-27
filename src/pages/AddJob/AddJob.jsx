f603a7d3d52043674153ea54ab4ffe2343b08ad6
Programming Hero <programming.hero1@gmail.com>
2025-05-27 10:49:09 -0500

    added more add a job field

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const AddJob = () => {
+
+    const handleAddAJob = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+        console.log(data);
+    }
+
     return (
         <div>
             <h2>Please add a job</h2>
-            <form>
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <form onSubmit={handleAddAJob}>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
                     <legend className="fieldset-legend">Basic Info</legend>
 
                     <label className="label">Job Title</label>
@@ -23,7 +32,7 @@ const AddJob = () => {
                 </fieldset>
 
                 {/* Job Type */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
                     <legend className="fieldset-legend">Job Type</legend>
                     <div className="filter">
                         <input className="btn filter-reset" type="radio" name="jobType" aria-label="All" />
@@ -34,7 +43,7 @@ const AddJob = () => {
 
                 </fieldset>
                 {/* Job Category */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
                     <legend className="fieldset-legend">Job Category</legend>
 
                     <select defaultValue="Job Category" name='category' className="select">
@@ -46,7 +55,7 @@ const AddJob = () => {
 
                 </fieldset>
                 {/* Application Deadline */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
                     <legend className="fieldset-legend">Application Deadline</legend>
 
                     <input type="date" className="input" />
@@ -54,23 +63,23 @@ const AddJob = () => {
                 </fieldset>
 
                 {/* Salary Range */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
                     <legend className="fieldset-legend">Salary Range</legend>
 
                     <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
                         <div>
                             <label className="label">Minimum Salary</label>
-                            <input type="text" name='salaryMin' className="input" placeholder="Minimum Salary" />
+                            <input type="text" name='min' className="input" placeholder="Minimum Salary" />
                         </div>
 
                         <div>
                             <label className="label">Maximum Salary</label>
-                            <input type="text" name='salaryMax' className="input" placeholder="Maximum Salary" />
+                            <input type="text" name='max' className="input" placeholder="Maximum Salary" />
                         </div>
 
                         <div>
                             <label className="label">Currency</label>
-                            <select defaultValue="Select a Currency" name='category' className="select">
+                            <select defaultValue="Select a Currency" name='currency' className="select">
                                 <option disabled={true}>Select a Currency</option>
                                 <option>BDT</option>
                                 <option>USD</option>
@@ -80,26 +89,40 @@ const AddJob = () => {
                     </div>
                 </fieldset>
 
-                {/*  */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <legend className="fieldset-legend">Basic Info</legend>
-
-
-                </fieldset>
-
-                {/*  */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <legend className="fieldset-legend">Basic Info</legend>
-
+                {/* Job Description  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+                    <legend className="fieldset-legend">Job Description</legend>
+                    <textarea className="textarea" name='description' placeholder="Job Description"></textarea>
 
                 </fieldset>
 
-                {/*  */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <legend className="fieldset-legend">Basic Info</legend>
+                {/* Job Requirements  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+                    <legend className="fieldset-legend">Job Requirements</legend>
 
+                    <textarea className="textarea" name='requirements' placeholder="Requirements (separate by comma)"></textarea>
 
                 </fieldset>
+
+                {/* Job Responsibilities  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+                    <legend className="fieldset-legend">Job Responsibilities</legend>
+
+                    <textarea className="textarea" name='responsibilities' placeholder="Responsibilities (separate by comma)"></textarea>
+
+                </fieldset>
+
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+                    <legend className="fieldset-legend">HR Related Info</legend>
+
+                    <label className="label">HR Name</label>
+                    <input type="text" name='hr_name' className="input" placeholder="HR Name" />
+
+                    <label className="label">HR Email</label>
+                    <input type="email" name='company' className="input" placeholder="HR Email" />
+                </fieldset>
+
+                <input type="submit" className='btn' value="Add Job" />
 
             </form>
         </div>
